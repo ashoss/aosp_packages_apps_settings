aosp-pb/packages_apps_settings

Settings: Add Misc settings preference
Place it inside "System" fragment

Change-Id: I47422f75e2f880bd877df91b60f62f844802c071
Signed-off-by: clarencelol <clarencekuiek@proton.me>

src/com/android/settings/aospb/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.aospb.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}

ashoss/aosp_packages_apps_settings

src/com/android/settings/custom/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.custom.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}