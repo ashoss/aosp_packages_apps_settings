aosp-pb/packages_apps_settings

Settings: Move battery styles to System/Miscellaneous fragment
Change-Id: I83d8547d26d237622b0a4d11d3e9c32d5a90d5ba

src/com/android/settings/aospb/misc/batteryCusts.java
@@ -1,0 +1,16 @@
+package com.android.settings.aospb.misc;
+import android.os.Bundle;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+public class batteryCusts extends SettingsPreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.battery_custs);
+    }
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}

ashoss/aosp_packages_apps_settings

src/com/android/settings/custom/misc/batteryCusts.java
@@ -1,0 +1,16 @@
+package com.android.settings.custom.misc;
+import android.os.Bundle;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+public class batteryCusts extends SettingsPreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.battery_custs);
+    }
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}