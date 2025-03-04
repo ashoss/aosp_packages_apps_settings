yaap/packages_apps_Settings

Settings: Allow customizing volume stream steps [2/2]
only media, calls and alarms seem fit
the rest are pretty much set and forget and don't need vol button granularity

Change-Id: I6ce6fa79df7b94ae84912945f6129048895067b2

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -174,6 +174,7 @@ import com.android.settings.security.SecurityAdvancedSettings;
 import com.android.settings.security.SecuritySettings;
 import com.android.settings.shortcut.CreateShortcut;
 import com.android.settings.sound.MediaControlsSettings;
+import com.android.settings.sound.VolumeSteps;
 import com.android.settings.support.SupportDashboardActivity;
 import com.android.settings.system.ResetDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
@@ -381,6 +382,7 @@ public class SettingsGateway {
             WifiTetherClientManager.class.getName(),
             WirelessDebuggingFragment.class.getName(),
             BatterySaverAdvancedSettings.class.getName(),
+            VolumeSteps.class.getName(),
             YaapSettings.class.getName()
     };
 

ashoss/aosp_packages_apps_settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -188,6 +188,7 @@ import com.android.settings.security.SecurityAdvancedSettings;
 import com.android.settings.security.SecuritySettings;
 import com.android.settings.shortcut.CreateShortcut;
 import com.android.settings.sound.MediaControlsSettings;
+import com.android.settings.sound.VolumeSteps;
 import com.android.settings.support.SupportDashboardActivity;
 import com.android.settings.system.ResetDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
@@ -401,7 +402,8 @@ public class SettingsGateway {
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
             ZenModesListFragment.class.getName(),
-            ZenModeFragment.class.getName()
+            ZenModeFragment.class.getName(),
+            VolumeSteps.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {